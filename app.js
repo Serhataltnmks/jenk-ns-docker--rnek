f686308bbf6837ee2d321edea17b5c6e321e26a7
Serhataltnmks <serhattaltinmakas.35@gmail.com>
2024-08-16 11:29:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-const http = require("http");
-const port = 3000;
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-const requestHandler = (request, response) => {
-  response.end("Merhabalar");
-}
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-const server = http.createServer(requestHandler);
-server.listen(port, () => {
-  console.log('Server is running on port ${port}');
-});
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    username = request.json['username']
+    email = request.json['email']
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(new_user)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    return users_schema.jsonify(all_users)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host='0.0.0.0', port=5003)
