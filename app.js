924f4855076a8dc32bc838ab7c62d0585b7da8a6
Serhataltnmks <serhattaltinmakas.35@gmail.com>
2024-08-18 00:59:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,27 @@ image: maven:3.8.6-eclipse-temurin-17
 stages:
   - build
   - test
-  - analyze
-
-cache:
-  paths:
-    - .m2/repository
+  - sonar
 
 variables:
-  SONAR_HOST_URL: "https://4dbd-188-119-27-46.ngrok-free.app"
-  SONAR_TOKEN: "sonar-token-ai"
+  SONAR_TOKEN: $SONAR_TOKEN_AI  # Sonar token GitLab CI/CD'den çekilecek
 
 build:
   stage: build
   script:
-    - echo "Maven cache cleared"
-    - mvn clean install
+    - echo "Building the project..."
+    - mvn clean package
 
 test:
   stage: test
   script:
-    - echo "Running tests"
+    - echo "Running tests..."
     - mvn test
 
-sonarqube-check:
-  stage: analyze
+sonar:
+  stage: sonar
   script:
-    - mvn sonar:sonar -Dsonar.projectKey=chess-ai-game -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+    - echo "Running SonarQube analysis..."
+    - mvn sonar:sonar -Dsonar.projectKey=chess-ai-game -Dsonar.host.url=https://4dbd-188-119-27-46.ngrok-free.app -Dsonar.login=$SONAR_TOKEN
   only:
     - master
